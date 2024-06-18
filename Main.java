0740ab019d02bb3d6cbdb92df2b421876ec86278
haru-15 <m1m24a33@oit.ac.jp>
2024-06-18 19:36:31 +0900

    version1.1の変更

--- Main.java
+++ Main.java
@@ -1,15 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-<<<<<<< HEAD
-    System.out.println("SEMonster!");
+    System.out.println("SEMONSTER GAME");
+    System.out.println("Show a new monster!");
     Player player = new Player();
     player.drawMonsters();
     player.showDeck();
-=======
-    System.out.println("SEMONSTER GAME");
-    System.out.println("Show a new monster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
->>>>>>> main
   }
 }
